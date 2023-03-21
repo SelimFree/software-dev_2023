05cf7daf09a88ad279027e59e824a39bb727124d
Selim Altayev <selim.altayev@gmail.com>
2023-03-21 22:18:43 +0100

    Finished login API methos

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,6 +6,8 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Api\UserModel;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -65,36 +67,46 @@ class UserController extends Controller
     }
 
 
-    public function login_get() {
-        return view('login_get');
-    }
 
+    public function login(Request $request) {
 
-    public function login_post(Request $request) {
+        if ($request->isMethod('get')) {
+            $user = Auth::user();
+            if ($user) {
+                return response()->json(UserModel::fromUser($user));
+            } else {
+                return response()->json(UserModel::fromError('Login needed'));
+            }
+        }
 
-        $credentials = $request->validate([
-            'email' => ['required'],
-            'password' => ['required']
+        $validator = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required'
         ]);
 
-        $selUser = User::where('email', $credentials['email'])->orWhere('username', $credentials['email'])->first();
+        if ($validator->fails()) {
+            return response()->json(UserModel::fromError('Login failed'));
+        }
 
+        $credentials = $validator->validated();
+
+        $selUser = User::query()->where('email', $credentials['email'])->orWhere('username', $credentials['email'])->first();
 
         if ($selUser) {
-
             $laravelAuthData = [
                 'email' => $selUser->email,
-                'password' => $credentials['password'],
+                'password' => $credentials['password']
             ];
 
             if (Auth::attempt($laravelAuthData)) {
                 $request->session()->regenerate();
+
                 return response()->json(UserModel::fromUser($selUser));
             } else {
-               return response()->json(UserModel::fromError("error 1"));
+                return response()->json(UserModel::fromError('Login failed'));
             }
-        } else{
-            return response()->json(UserModel::fromError("error 2"));
+        } else {
+            return response()->json(UserModel::fromError('Login failed'));
         }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +16,3 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/login', [UserController::class, 'login_get']);
-Route::post('/login', [UserController::class, 'login_post']);
